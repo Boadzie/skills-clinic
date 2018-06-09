8448cdf9627fdf546639cd7efc52ba53f23686ad
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-06-09 04:32:18 +0000

    Updated content for components

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,47 +1,50 @@
-import React from 'react';
-import './Card.css';
-import notebook from './notebook.jpg';
+import React from "react";
+import "./Card.css";
+import notebook from "./notebook.jpg";
 
 const Card = () => {
-    return(
-        <section className="section-card">
-            <div class="row">
-                <div className="card">
-                    <img src={notebook} alt="Discover" />
-                    <div className="description">
-                        <h3 className="title">Title</h3>
-                        <p className="text">
-                            reate React App is the best way to start building a new React single page application.
-                        </p>
-                        <a href="" className="card-btn">Learn More</a>
-                    </div>
-                    
-                </div>
-                <div className="card">
-                    <img src={notebook} alt="Discover" />
-                    <div className="description">
-                        <h3 className="title">Title</h3>
-                        <p className="text">
-                            reate React App is the best way to start building a new React single page application.
-                        </p>
-                        <a href="" className="card-btn">Learn More</a>
-                    </div>
-                    
-                </div>
-                <div className="card">
-                    <img src={notebook} alt="Discover" />
-                    <div className="description">
-                        <h3 className="title">Title</h3>
-                        <p className="text">
-                            reate React App is the best way to start building a new React single page application.
-                        </p>
-                        <a href="" className="card-btn">Learn More</a>
-                    </div>
-                    
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="section-card">
+      <div class="row">
+        <div className="card">
+          <img src={notebook} alt="Discover" />
+          <div className="description">
+            <h3 className="title">Discover</h3>
+            <p className="text">
+              Discover all of your potentials and passions.
+            </p>
+            <a href="" className="card-btn">
+              Learn More
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img src={notebook} alt="Discover" />
+          <div className="description">
+            <h3 className="title">Develop</h3>
+            <p className="text">
+              Develop your potentials by acquiring 21st Century skills
+            </p>
+            <a href="" className="card-btn">
+              Learn More
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img src={notebook} alt="Discover" />
+          <div className="description">
+            <h3 className="title">Use</h3>
+            <p className="text">
+              Use your potentials to serve the world through Entrepreneurship
+            </p>
+            <a href="" className="card-btn">
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Card;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header>
-            <Nav />
-            <div className="header-text">
-                <h1>21st Century Skills</h1>
-                <a href='' className="btn red">Discover</a>
-                <a href='' className="btn blue">Develop</a>
-                <a href='' className="btn green">Use</a>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header>
+      <Nav />
+      <div className="header-text">
+        <h1>21st Century Skills Clinic</h1>
+        <a href="" className="btn red">
+          Discover
+        </a>
+        <a href="" className="btn blue">
+          Develop
+        </a>
+        <a href="" className="btn green">
+          Use
+        </a>
+      </div>
+    </header>
+  );
+};
 
 const Nav = () => {
-    return (
-        <nav>
-            <a href=""><span>21st</span> Century Skills</a>
-            <ul className="main-nav">
-                <li><a href=''>Sign Up</a></li>
-                <li><a href=''>Login</a></li>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <a href="">
+        <span>21st</span> Century Skills
+      </a>
+      <ul className="main-nav">
+        <li>
+          <a href="">Sign Up</a>
+        </li>
+        <li>
+          <a href="">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Header;
