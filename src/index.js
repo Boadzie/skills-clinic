1c28d508d939d0e30f54ea3d72935af35c1aa00d
Victor Bruce <victorbruce82@gmail.com>
2018-06-09 00:22:43 +0000

    Added cards to home page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import Header from './component/Header';
 import Section from './component/Section';
 import Footer from './component/Footer';
+import Card from './component/Card';
 
 ReactDOM.render(
     <div>
         <Header /> 
         <Section />
+        <Card />
         <Footer />
     </div>,
     document.getElementById('root')
